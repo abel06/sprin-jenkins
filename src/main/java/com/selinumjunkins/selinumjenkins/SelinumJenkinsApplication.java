d40b099fdb54f4f651720ea20eedfaecb3433aec
abel <abelmesfin2003@gmail.com>
2022-05-04 14:52:59 +0200

    Initial commit

---
+++ src/main/java/com/selinumjunkins/selinumjenkins/SelinumJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.selinumjunkins.selinumjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelinumJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelinumJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selinumjunkins/selinumjenkins/SelinumJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selinumjunkins.selinumjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelinumJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
